95ee7b9d15b1980d721035fb4cb69e7f20f60262
Teddy Heinen <skyshayde@gmail.com>
2019-09-09 09:08:58 -0500

    uhh quite a bit of stuff because i haven't committed in ages

--- static/script.js
+++ static/script.js
@@ -1,9 +1,28 @@
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    var elem = document.querySelector('.grid');
-    var pckry = new Packery( elem, {
-      // options
-      itemSelector: '.grid-item',
-      gutter: 10
-    });
+var pckry = new Packery( '.grid', {
+  // options
+  itemSelector: '.grid-item',
+  gutter: 10
+});
+
+Packery.prototype.refresh = function() {
+    this.reloadItems();
+    this.layout();
+}
+
+$('#interests-menu').click(function() {
+    $('.interests').prependTo($('.grid'));
+    pckry.refresh()
+});
+$('#experience-menu').click(function() {
+    $('.experience').prependTo($('.grid'));
+    pckry.refresh()
+});
+$('#skills-menu').click(function() {
+    $('.skills').prependTo($('.grid'));
+    pckry.refresh()
+});
+$('#service-menu').click(function() {
+    $('.service').prependTo($('.grid'));
+    pckry.refresh()
 });
